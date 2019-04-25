498ee45eeee92e7d7fbd362fda93564e664fcfe8
Test Nicolas <nicolas.gennart@hotmail.com>
2019-04-25 14:45:25 +0200

    message

--- alexa.py
+++ alexa.py
@@ -1,5 +1,21 @@
-<<<<<<< HEAD
 
-=======
-print ("salut")
->>>>>>> 0022536fb5c5bf8dbc23b64c198419d4601c9e4e
+def main():
+    i = split1(read_lines("fin.txt"))
+    print(i[0])
+
+def read_lines(filename):
+    try:
+        f1 = open(filename, "r")
+        lines = f1.read()
+    finally:
+        f1.close()
+    return lines
+
+def split1(fichier):
+    ligne = fichier.split("#")
+    for i in 10:
+
+    return x
+
+if __name__=='__main__':
+    main()
